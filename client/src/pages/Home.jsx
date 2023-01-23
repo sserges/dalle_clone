05c9e6bae2b5c5cc595cef3bd85a297e229551ec
sserges <agounkeserge@gmail.com>
2023-01-23 19:59:04 -0400

    HomePage

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -41,7 +41,13 @@ const Home = () => {
                             Showing results for <span className='text-[#222328]'>{searchText}</span>
                         </h2>
                     )}
-                    <div className='grid lg:grid-cols-4 sm:grid-cols-3 xs:grid-cols-2 grid-cols-1 gap-3'></div>
+                    <div className='grid lg:grid-cols-4 sm:grid-cols-3 xs:grid-cols-2 grid-cols-1 gap-3'>
+                        {searchText ? (
+                            <RenderCards data={[]} title="No search results found" />
+                        ) : (
+                            <RenderCards data={[]} title="No posts found" />
+                        )}
+                    </div>
                 </>}
             </div>
         </section>
