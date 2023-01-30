49ce67df9c758d851ac1b795337774ad9e839003
sserges <agounkeserge@gmail.com>
2023-01-30 19:10:03 -0400

    CreatePost updated

--- client/src/pages/CreatePost.jsx
+++ client/src/pages/CreatePost.jsx
@@ -24,11 +24,12 @@ const CreatePost = () => {
     }
 
     const handleChange = (e) => {
-
+        setForm({ ...form, [e.target.name]: e.target.value })
     }
 
     const handleSupriseMe = () => {
-
+        const randomPrompt = getRandomPrompt(form.prompt)
+        setForm({ ...form, prompt: randomPrompt })
     }
 
     return (
@@ -83,7 +84,23 @@ const CreatePost = () => {
                 </div>
 
                 <div className='mt-5 flex gap-5'>
-                    <button type='button' onClick={generateImage}></button>
+                    <button
+                        type='button'
+                        onClick={generateImage}
+                        className="text-white bg-green-700 font-medium rounded-md text-sm w-full sm:w-auto px-5 py-2.5 text-center"
+                    >
+                        {generatingImg ? 'Generating...' : 'Generate'}
+                    </button>
+                </div>
+
+                <div className='mt-10'>
+                    <p className='mt-2 text-[#666e75] text-[14px]'>Once you have created the image you want, you can share it with others in the community</p>
+                    <button
+                        type='submit'
+                        className='mt-3 text-white bg-[#6469ff] font-medium rounded-md text-sm w-full sm:w-auto px-5 py-2.5 text-center'
+                    >
+                        {loading ? 'Sharing...' : 'Share with the community'}
+                    </button>
                 </div>
             </form>
         </section>
