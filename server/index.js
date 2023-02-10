b066573cd288bbe2096ac1942a3e1d8d6ce3689e
sserges <agounkeserge@gmail.com>
2023-02-10 18:55:10 -0400

    MongoDB Setup

--- server/index.js
+++ server/index.js
@@ -2,6 +2,11 @@ import express from 'express'
 import * as dotenv from 'dotenv'
 import cors from 'cors'
 
+import connectDB from './mongodb/connect.js'
+
+import postRoutes from './routes/postRoutes.js'
+import dalleRoutes from './routes/dalleRoutes.js'
+
 dotenv.config()
 
 const PORT = process.env.PORT || 8080
@@ -10,14 +15,22 @@ const app = express()
 app.use(cors())
 app.use(express.json({ limit: '50mb' }))
 
+app.use('/api/v1/post', postRoutes)
+app.use('/api/v1/dalle', dalleRoutes)
+
 app.get('/', async (req, res) => {
   res.send('Hello from DALL-E')
 })
 
 const startServer = async () => {
-  app.listen(PORT, () =>
-    console.log(`Server has started on port http://localhost:${PORT}`)
-  )
+  try {
+    connectDB(process.env.MONGODB_URL)
+    app.listen(PORT, () =>
+      console.log(`Server has started on port http://localhost:${PORT}`)
+    )
+  } catch (err) {
+    console.log(err)
+  }
 }
 
 startServer()
