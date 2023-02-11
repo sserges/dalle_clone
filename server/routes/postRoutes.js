99a83aedc8eb0e341db41ea7f76afcbf3257d900
sserges <agounkeserge@gmail.com>
2023-02-11 17:00:24 -0400

    createImage route added

---
+++ server/routes/postRoutes.js
@@ -0,0 +1,8 @@
+import express from 'express'
+import { v2 as cloudinary } from 'cloudinary'
+
+import Post from '../mongodb/models/post.js'
+
+const router = express.Router()
+
+export default router
